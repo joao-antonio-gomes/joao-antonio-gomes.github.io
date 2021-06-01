45a5700f6e590e733f42db4ae67ea36ac713235c
João Gomes <joo.gomes@olsen.odo.br>
2021-06-01 11:29:41 -0300

    .

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
     })
 });
 
-
 particlesJS.load('particles-js', 'assets/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
